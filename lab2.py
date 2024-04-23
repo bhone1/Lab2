7a5d47a1c3522bbae9d1cc2e32c9616ebdd2129c
bhoneha <bhonehtetaung28655@gmail.com>
2024-04-23 13:08:27 +0800

    Create a bmi calculator and classifier

--- lab2.py
+++ lab2.py
@@ -1,1 +1,13 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(weight, height):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / (height**2)
+    print(f"BMI : {bmi}")
+    if bmi < 18.5:
+        print("Under Weight")
+    elif bmi >= 18.5 and bmi <= 25.0:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+calculate_bmi(57, 1.73)
