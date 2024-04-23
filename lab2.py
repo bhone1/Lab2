831c404a0c81bafb81c7d3d12344fcc289a5a87e
bhoneha <bhonehtetaung28655@gmail.com>
2024-04-23 13:46:54 +0800

    Exercise 2,3,4,5

--- lab2.py
+++ lab2.py
@@ -1,3 +1,38 @@
+def main():
+    calculate_bmi(1,1)
+
+
+def display_main_menu():
+    print("Enter some numbers separated by commas(e.g. 5, 67, 32)")
+
+def get_user_input():
+    user_input = input()
+    #split the string into a list
+    user_input_list = user_input.split(",")
+    for i in range(len(user_input_list)):
+        user_input_list[i] = float(user_input_list[i])
+    return user_input_list
+
+
+
+
+def calc_average_temperature(tempr_list):
+    for x in tempr_list:
+        total += x
+    return total / len(tempr_list)
+
+def calc_min_max_temperature(tempr_list):
+    sort_temperature()
+    min = tempr_list[0]
+    max = tempr_list[-1]
+    return [min, max]
+
+def sort_temperature(list):
+    list.sort()
+
+def calc_median_temperature(list):
+    return 1
+
 def calculate_bmi(weight, height):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
@@ -10,4 +45,9 @@ def calculate_bmi(weight, height):
     else:
         print("Over Weight")
 
-calculate_bmi(57, 1.73)
+
+
+
+
+if __name__ == "__main__":
+    main()
