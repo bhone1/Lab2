afeb722672852392e7ad5e1e35827e4525d16eea
bhoneha <bhonehtetaung28655@gmail.com>
2024-04-30 12:59:10 +0800

    Modified calculate_bmi() to return values

--- lab2.py
+++ lab2.py
@@ -44,19 +44,6 @@ def calc_median_temperature(list):
         median2 = list[int(length / 2)]
         return (median1 + median2) / 2
 
-def calculate_bmi(weight, height):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    bmi = weight / (height**2)
-    print(f"BMI : {bmi}")
-    if bmi < 18.5:
-        print("Under Weight")
-    elif bmi >= 18.5 and bmi <= 25.0:
-        print("Normal Weight")
-    else:
-        print("Over Weight")
-
-
 
 
 
