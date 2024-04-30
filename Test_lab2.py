bac4b333071388d2d3c6d1893282db27a66a2b75
bhone1 <bhonehtet.23@ichat.sp.edu.sg>
2024-04-30 15:26:43 +0800

    testlab2

---
+++ Test_lab2.py
@@ -0,0 +1,13 @@
+import lab2.lab2 as lab2
+
+def test_min_max():
+    test_input = [10, 2, 38, 23, 38, 23, 21]
+    assert(lab2.calc_min_max_temperature(test_input) == [2,38])
+
+def test_calc_avg():
+    test_input = [10, 2, 38, 23, 38, 23, 21]
+    assert(lab2.calc_average_temperature(test_input) == 22.14)
+
+def test_calc_median():
+    test_input = [10, 2, 38, 23, 38, 23, 21]
+    assert(lab2.calc_median_temperature() == 23)
