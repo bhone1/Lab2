d5e5138294c73659ded0b56f1730a8e68b833ef8
bhone1 <bhonehtet.23@ichat.sp.edu.sg>
2024-05-01 01:28:28 +0800

    updated test lab2

--- Test_lab2.py
+++ Test_lab2.py
@@ -9,6 +9,7 @@ def test_calc_avg():
     test_input = [10, 2, 38, 23, 38, 23, 21]
     assert(lab2.calc_average_temperature(test_input) == 22.14)
 
+
 def test_calc_median():
     test_input = [10, 2, 38, 23, 38, 23, 21]
     assert(lab2.calc_median_temperature(test_input) == 23)
